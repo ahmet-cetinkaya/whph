a473970be2f8ea425700b7179ea770d060d1aedf
Ahmet Çetinkaya <ahmetcetinkaya@tutamail.com>
2024-09-01 21:06:38 +0300

    refactor(tasks): add task form and task list components
    The code changes introduce two new components: `TaskForm` and `TasksList`. The `TaskForm` component is responsible for rendering a form to add new tasks, while the `TasksList` component displays a list of tasks. These components are essential for managing tasks in the application.
    
    This commit message follows the established convention of using a prefix to indicate the type of change (`feat` for feature).

---
+++ lib/presentation/features/tasks/components/tasks_list.dart
@@ -0,0 +1,103 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:mediatr/mediatr.dart';
+import 'package:whph/application/features/tasks/queries/get_list_tasks_query.dart';
+import 'package:whph/presentation/features/tasks/components/task_card.dart';
+
+class TasksList extends StatefulWidget {
+  final Mediator mediator;
+  final VoidCallback onTaskAdded;
+  final void Function(TaskListItem task) onClickTask;
+
+  const TasksList({
+    super.key,
+    required this.mediator,
+    required this.onTaskAdded,
+    required this.onClickTask,
+  });
+
+  @override
+  _TasksListState createState() => _TasksListState();
+}
+
+class _TasksListState extends State<TasksList> {
+  List<TaskListItem> _tasks = [];
+  int _pageIndex = 0;
+  bool _hasNext = false;
+  final ScrollController _scrollController = ScrollController();
+  int _loadingCount = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchTasks();
+    _setupScrollListener();
+  }
+
+  Future<void> _fetchTasks({int pageIndex = 0}) async {
+    setState(() {
+      _loadingCount++;
+    });
+
+    const pageSize = 20;
+    var query = GetListTasksQuery(pageIndex: pageIndex, pageSize: pageSize);
+    var queryResponse = await widget.mediator.send<GetListTasksQuery, GetListTasksQueryResponse>(query);
+
+    setState(() {
+      _tasks = [..._tasks, ...queryResponse.items];
+      _pageIndex = pageIndex;
+      _hasNext = queryResponse.hasNext;
+      _loadingCount--;
+    });
+  }
+
+  void _setupScrollListener() {
+    _scrollController.addListener(() async {
+      if (_scrollController.position.pixels == _scrollController.position.maxScrollExtent && _hasNext) {
+        await _fetchTasks(pageIndex: _pageIndex + 1);
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return RefreshIndicator(
+      onRefresh: () async {
+        setState(() {
+          _tasks.clear();
+          _pageIndex = 0;
+        });
+        await _fetchTasks();
+      },
+      child: ListView.builder(
+        controller: _scrollController,
+        itemCount: _tasks.length + (_loadingCount > 0 ? 1 : 0),
+        itemBuilder: (context, index) {
+          if (_loadingCount > 0) {
+            return const Center(
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 8.0),
+                child: CircularProgressIndicator(),
+              ),
+            );
+          }
+
+          final task = _tasks[index];
+          return TaskCard(
+            task: task,
+            onOpenDetails: () {
+              widget.onClickTask(task); // Use the passed callback here
+            },
+            onCompleted: () {
+              setState(() {
+                _tasks.clear();
+              });
+              _fetchTasks();
+            },
+          );
+        },
+      ),
+    );
+  }
+}
--- lib/presentation/features/tasks/pages/tasks_page.dart
+++ lib/presentation/features/tasks/pages/tasks_page.dart
@@ -1,11 +1,9 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:mediatr/mediatr.dart';
-import 'package:whph/application/features/tasks/commands/save_task_command.dart';
 import 'package:whph/application/features/tasks/queries/get_list_tasks_query.dart';
 import 'package:whph/main.dart';
-import 'package:whph/presentation/features/tasks/components/task_card.dart';
+import 'package:whph/presentation/features/tasks/components/task_add_form.dart';
+import 'package:whph/presentation/features/tasks/components/tasks_list.dart';
 import 'package:whph/presentation/features/tasks/pages/task_details_page.dart';
 
 class TasksPage extends StatefulWidget {
@@ -18,72 +16,23 @@ class TasksPage extends StatefulWidget {
 }
 
 class _TasksPageState extends State<TasksPage> {
-  List<TaskListItem> _tasks = [];
-  int _pageIndex = 0;
-  bool _hasNext = false;
-  final ScrollController _scrollController = ScrollController();
-  int _loadingCount = 0;
-  final TextEditingController _taskTitleController = TextEditingController();
+  Key _tasksListKey = UniqueKey();
 
-  @override
-  void initState() {
-    super.initState();
-    _fetchTasks();
-    _setupScrollListener();
-  }
-
-  Future<void> _fetchTasks({int pageIndex = 0}) async {
+  void _refreshTasks() {
     setState(() {
-      _loadingCount++;
-    });
-
-    const pageSize = 20;
-    var query = GetListTasksQuery(pageIndex: pageIndex, pageSize: pageSize);
-    var queryResponse = await widget.mediator.send<GetListTasksQuery, GetListTasksQueryResponse>(query);
-
-    setState(() {
-      _tasks = [..._tasks, ...queryResponse.items];
-      _pageIndex = pageIndex;
-      _hasNext = queryResponse.hasNext;
-      _loadingCount--;
+      // Change the key to force TasksList to rebuild
+      _tasksListKey = UniqueKey();
     });
   }
 
-  void _setupScrollListener() {
-    _scrollController.addListener(() async {
-      if (_scrollController.position.pixels == _scrollController.position.maxScrollExtent && _hasNext) {
-        await _fetchTasks(pageIndex: _pageIndex + 1);
-      }
-    });
-  }
-
-  Future<void> _openDetails(TaskListItem task) async {
+  Future<void> _openTaskDetails(TaskListItem task) async {
     await Navigator.push(
       context,
       MaterialPageRoute(
         builder: (context) => TaskDetailsPage(taskId: task.id),
       ),
     );
-    _tasks.clear();
-    _fetchTasks();
-  }
-
-  Future<void> _addTask() async {
-    final title = _taskTitleController.text.trim();
-    if (title.isEmpty) {
-      return;
-    }
-
-    var command = SaveTaskCommand(title: title);
-    await widget.mediator.send<SaveTaskCommand, SaveTaskCommandResponse>(command);
-
-    setState(() {
-      _tasks.clear();
-      _pageIndex = 0;
-    });
-    await _fetchTasks();
-
-    _taskTitleController.clear();
+    _refreshTasks(); // Refresh tasks after returning from details
   }
 
   @override
@@ -94,74 +43,22 @@ class _TasksPageState extends State<TasksPage> {
         actions: [
           IconButton(
             icon: const Icon(Icons.refresh),
-            onPressed: () async {
-              setState(() {
-                _tasks.clear();
-                _pageIndex = 0;
-              });
-              await _fetchTasks();
-            },
+            onPressed: _refreshTasks,
           ),
         ],
       ),
       body: Column(
         children: [
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    controller: _taskTitleController,
-                    decoration: const InputDecoration(
-                      hintText: 'Enter task title',
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
-                ),
-                IconButton(
-                  icon: const Icon(Icons.add),
-                  onPressed: _addTask,
-                ),
-              ],
-            ),
+          TaskForm(
+            mediator: widget.mediator,
+            onTaskAdded: _refreshTasks, // Notify to refresh the task list
           ),
           Expanded(
-            child: RefreshIndicator(
-              onRefresh: () async {
-                setState(() {
-                  _tasks.clear();
-                });
-                await _fetchTasks();
-              },
-              child: ListView.builder(
-                controller: _scrollController,
-                itemCount: _tasks.length + (_loadingCount > 0 ? 1 : 0),
-                itemBuilder: (context, index) {
-                  if (_loadingCount > 0) {
-                    return const Center(
-                      child: Padding(
-                        padding: EdgeInsets.symmetric(vertical: 8.0),
-                        child: CircularProgressIndicator(),
-                      ),
-                    );
-                  }
-
-                  final task = _tasks[index];
-                  return TaskCard(
-                    task: task,
-                    onOpenDetails: () {
-                      _openDetails(task);
-                    },
-                    onCompleted: () {
-                      setState(() {
-                        _tasks.clear();
-                      });
-                      _fetchTasks();
-                    },
-                  );
-                },
-              ),
+            child: TasksList(
+              key: _tasksListKey, // Assign the key to TasksList
+              mediator: widget.mediator,
+              onTaskAdded: _refreshTasks,
+              onClickTask: _openTaskDetails,
             ),
           ),
         ],
