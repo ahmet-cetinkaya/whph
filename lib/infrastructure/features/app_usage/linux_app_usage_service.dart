c3748cc2decc623663ee47baf4b5bfe8f6f23267
Ahmet Çetinkaya <ahmetcetinkaya@tutamail.com>
2025-01-25 01:15:18 +0300

    fix(app_usage): enhance Linux and Windows services to dynamically locate and execute active window scripts

--- lib/infrastructure/features/app_usage/linux_app_usage_service.dart
+++ lib/infrastructure/features/app_usage/linux_app_usage_service.dart
@@ -1,4 +1,6 @@
 import 'dart:io';
+import 'package:flutter/foundation.dart';
+import 'package:path/path.dart' as path;
 import 'base_desktop_app_usage_service.dart';
 
 class LinuxAppUsageService extends BaseDesktopAppUsageService {
@@ -10,10 +12,34 @@ class LinuxAppUsageService extends BaseDesktopAppUsageService {
     super.settingRepository,
   );
 
+  String get _scriptPath {
+    final exePath = Platform.resolvedExecutable;
+    final exeDir = path.dirname(exePath);
+    final scriptPath = path.join(exeDir, 'data', 'flutter_assets', 'linux', 'getActiveWindow.bash');
+    return scriptPath;
+  }
+
   @override
   Future<String?> getActiveWindow() async {
-    const scriptPath = 'linux/getActiveWindow.bash';
-    final result = await Process.run('bash', ["${Directory.current.path}/$scriptPath"]);
-    return result.stdout.trim();
+    try {
+      if (!File(_scriptPath).existsSync()) {
+        if (kDebugMode) print('ERROR: Script not found at: $_scriptPath');
+        return null;
+      }
+
+      await Process.run('chmod', ['+x', _scriptPath]);
+
+      final result = await Process.run('bash', [_scriptPath]);
+
+      if (result.exitCode != 0) {
+        if (kDebugMode) print('ERROR: Bash script error: ${result.stderr}');
+        return null;
+      }
+
+      return result.stdout.trim();
+    } catch (e) {
+      if (kDebugMode) print('ERROR: Error running Bash script: $e');
+      return null;
+    }
   }
 }
