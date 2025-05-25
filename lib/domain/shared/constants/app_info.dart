66602e63ed1b98bf4bfaed60cd8d896f3f44d405
Ahmet Çetinkaya <ahmetcetinkaya@tutamail.com>
2025-05-26 01:59:52 +0300

    chore: update version to 0.6.0
    Bump the app version to 0.6.0 in both app_info.dart
    and pubspec.yaml to reflect the latest changes and
    ensure consistency across the project.

--- lib/domain/shared/constants/app_info.dart
+++ lib/domain/shared/constants/app_info.dart
@@ -3,11 +3,12 @@ import 'package:whph/domain/shared/constants/app_assets.dart';
 class AppInfo {
   static const String name = "Work Hard Play Hard";
   static const String shortName = "WHPH";
-  static const String version = "0.5.1";
+  static const String version = "0.6.0";
   static const String websiteUrl = "https://whph.ahmetcetinkaya.me/";
   static const String sourceCodeUrl = "https://github.com/ahmet-cetinkaya/whph";
   static const String logoPath = AppAssets.logo;
   static const String supportUrl = "https://ahmetcetinkaya.me/donate";
   static const String updateCheckerUrl = "https://api.github.com/repos/ahmet-cetinkaya/whph/releases/latest";
   static const String supportEmail = "contact@ahmetcetinkaya.me";
+  static const String feedbackUrl = "https://github.com/ahmet-cetinkaya/whph/issues";
 }
