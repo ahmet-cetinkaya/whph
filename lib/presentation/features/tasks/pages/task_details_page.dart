a23899e401eac45d66f88c40f196bdf1f6222dab
Ahmet Çetinkaya <ahmetcetinkaya@tutamail.com>
2025-05-28 18:49:27 +0300

    fix(tasks): add sorting functionality to task lists in detail pages
    Implement sorting options for tasks in the detail pages.
    This enhances user experience by allowing users to sort tasks
    based on their preferences, improving task management efficiency.

--- lib/presentation/features/tasks/pages/task_details_page.dart
+++ lib/presentation/features/tasks/pages/task_details_page.dart
@@ -3,17 +3,20 @@ import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:mediatr/mediatr.dart';
 import 'package:whph/application/features/tasks/queries/get_task_query.dart';
+import 'package:whph/application/features/tasks/queries/get_list_tasks_query.dart';
 import 'package:whph/main.dart';
 import 'package:whph/presentation/features/tasks/components/task_add_button.dart';
+import 'package:whph/presentation/features/tasks/components/task_delete_button.dart';
 import 'package:whph/presentation/features/tasks/components/task_list_options.dart';
 import 'package:whph/presentation/features/tasks/components/tasks_list.dart';
-import 'package:whph/presentation/shared/constants/app_theme.dart';
-import 'package:whph/presentation/features/tasks/components/task_delete_button.dart';
 import 'package:whph/presentation/features/tasks/components/task_details_content.dart';
+import 'package:whph/presentation/features/tasks/constants/task_defaults.dart';
 import 'package:whph/presentation/features/tasks/constants/task_translation_keys.dart';
 import 'package:whph/presentation/shared/components/help_menu.dart';
-import 'package:whph/presentation/shared/services/abstraction/i_translation_service.dart';
 import 'package:whph/presentation/shared/utils/responsive_dialog_helper.dart';
+import 'package:whph/presentation/shared/constants/app_theme.dart';
+import 'package:whph/presentation/shared/models/sort_config.dart';
+import 'package:whph/presentation/shared/services/abstraction/i_translation_service.dart';
 
 class TaskDetailsPage extends StatefulWidget {
   static const String route = '/tasks/details';
@@ -41,7 +44,9 @@ class _TaskDetailsPageState extends State<TaskDetailsPage> with AutomaticKeepAli
   Key _listRebuildKey = UniqueKey();
   final _translationService = container.resolve<ITranslationService>();
 
+  // Task filter options
   String? _searchQuery;
+  SortConfig<TaskSortFields> _taskSortConfig = TaskDefaults.sorting;
   bool _isRefreshInProgress = false;
   Timer? _debounceTimer;
 
@@ -246,9 +251,17 @@ class _TaskDetailsPageState extends State<TaskDetailsPage> with AutomaticKeepAli
                               });
                               _refreshTasksList();
                             },
+                            onSortChange: (newConfig) {
+                              setState(() {
+                                _taskSortConfig = newConfig;
+                              });
+                              _refreshTasksList();
+                            },
                             hasItems: true,
                             showDateFilter: false,
                             showTagFilter: false,
+                            showSortButton: true,
+                            sortConfig: _taskSortConfig,
                             settingKeyVariantSuffix: subTaskFilterOptionsSettingKeySuffix,
                           )
                         ],
@@ -300,7 +313,8 @@ class _TaskDetailsPageState extends State<TaskDetailsPage> with AutomaticKeepAli
                         _refreshEverything();
                       },
                       onScheduleTask: (_, __) => _refreshEverything(),
-                      enableReordering: !_showCompletedTasks,
+                      enableReordering: !_showCompletedTasks && _taskSortConfig.useCustomOrder,
+                      sortConfig: _taskSortConfig,
                     ),
                   ),
                 ),
