9e3f36dfde153beb8d3a2aa75abc03e6427530ad
Ahmet Çetinkaya <ahmetcetinkaya@tutamail.com>
2025-07-02 13:12:10 +0300

    refactor: update sound files to opus format
    Replaced mp3 sound files with their opus counterparts for
    better audio quality and performance. Updated references
    in SharedSounds class accordingly.

--- lib/src/presentation/ui/shared/constants/shared_sounds.dart
+++ lib/src/presentation/ui/shared/constants/shared_sounds.dart
@@ -1,5 +1,5 @@
 class SharedSounds {
-  static const String done = "lib/src/presentation/ui/shared/assets/sounds/level-up-191997.mp3";
-  static const String alarmDone = "lib/src/presentation/ui/shared/assets/sounds/level-completed-90734.mp3";
-  static const String button = "lib/src/presentation/ui/shared/assets/sounds/button-202966.mp3";
+  static const String done = "lib/src/presentation/ui/shared/assets/sounds/level-up-191997.opus";
+  static const String alarmDone = "lib/src/presentation/ui/shared/assets/sounds/level-completed-90734.opus";
+  static const String button = "lib/src/presentation/ui/shared/assets/sounds/button-202966.opus";
 }
