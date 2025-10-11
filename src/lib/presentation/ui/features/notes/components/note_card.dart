0a1f247ae4d8807454641deb117f4def22aa116c
Ahmet Çetinkaya <ahmetcetinkaya@tutamail.com>
2025-10-11 16:28:47 +0300

    fix(ui): refactor tag display logic in note_card.dart

--- src/lib/presentation/ui/features/notes/components/note_card.dart
+++ src/lib/presentation/ui/features/notes/components/note_card.dart
@@ -99,40 +99,38 @@ class _NoteCardState extends State<NoteCard> {
       return const SizedBox.shrink();
     }
 
-    // Limit the length of tag names to prevent overflow and limit to 5 tags to prevent too many
-    final List<String> tagNames = widget.note.tags.length > 5
-        ? widget.note.tags
-            .take(5)
-            .map((tag) => tag.tagName.isNotEmpty
-                ? (tag.tagName.length > 20 ? '${tag.tagName.substring(0, 17)}...' : tag.tagName)
-                : _translationService.translate(SharedTranslationKeys.untitled))
-            .toList()
-        : widget.note.tags
-            .map((tag) => tag.tagName.isNotEmpty
-                ? (tag.tagName.length > 20 ? '${tag.tagName.substring(0, 17)}...' : tag.tagName)
-                : _translationService.translate(SharedTranslationKeys.untitled))
-            .toList();
+    const int maxTagsToShow = 5;
+    const int maxTagNameLength = 20;
+    const int truncatedTagNameLength = 17;
+
+    final bool hasMoreTags = widget.note.tags.length > maxTagsToShow;
+    final tagsToProcess = hasMoreTags ? widget.note.tags.take(maxTagsToShow) : widget.note.tags;
+
+    final List<String> tagNames = tagsToProcess
+        .map((tag) => tag.tagName.isNotEmpty
+            ? (tag.tagName.length > maxTagNameLength ? '${tag.tagName.substring(0, truncatedTagNameLength)}...' : tag.tagName)
+            : _translationService.translate(SharedTranslationKeys.untitled))
+        .toList();
 
     // Add a "+X more" indicator if there are more than 5 tags
-    if (widget.note.tags.length > 5) {
-      final int extraCount = widget.note.tags.length - 5;
+    if (hasMoreTags) {
+      final int extraCount = widget.note.tags.length - maxTagsToShow;
       tagNames.add('+$extraCount more');
+    }
+
+    final List<Color> tagColors = tagsToProcess
+        .map((tag) => tag.tagColor != null ? Color(int.parse('FF${tag.tagColor}', radix: 16)) : Colors.grey)
+        .toList();
+
+    if (hasMoreTags) {
+      tagColors.add(Colors.grey); // color for "+X more" text
     }
 
     return Label.multipleColored(
       icon: TagUiConstants.tagIcon,
       color: Colors.grey,
       values: tagNames,
-      colors: widget.note.tags.length > 5
-          ? [
-              ...widget.note.tags
-                  .take(5)
-                  .map((tag) => tag.tagColor != null ? Color(int.parse('FF${tag.tagColor}', radix: 16)) : Colors.grey),
-              Colors.grey // color for "+X more" text
-            ]
-          : widget.note.tags
-              .map((tag) => tag.tagColor != null ? Color(int.parse('FF${tag.tagColor}', radix: 16)) : Colors.grey)
-              .toList(),
+      colors: tagColors,
       mini: true,
     );
   }
