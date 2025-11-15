8ea8cc0b928c421f000d06f6b762fcde757b1ee3
Ahmet Çetinkaya <ahmetcetinkaya@tutamail.com>
2025-11-15 18:55:37 +0300

    refactor(ui): remove unused ISoundPlayer import from habit details component

--- src/lib/presentation/ui/features/habits/components/habit_details_content.dart
+++ src/lib/presentation/ui/features/habits/components/habit_details_content.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:mediatr/mediatr.dart';
-import 'package:acore/acore.dart' show NumericInput, DateTimeHelper, ISoundPlayer;
+import 'package:acore/acore.dart' show NumericInput, DateTimeHelper;
 import 'package:whph/corePackages/acore/lib/components/numeric_input/numeric_input_translation_keys.dart';
 import 'package:whph/presentation/ui/shared/services/abstraction/i_sound_manager_service.dart';
 import 'package:whph/presentation/ui/shared/components/markdown_editor.dart';
